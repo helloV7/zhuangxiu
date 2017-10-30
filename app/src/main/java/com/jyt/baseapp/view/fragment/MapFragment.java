6b6796c078d7bf403850f6e2183f664a9ff720d6
hongguangqq <1154716396@qq.com>
2017-10-30 19:23:50 +0800

    one day

--- app/src/main/java/com/jyt/baseapp/view/fragment/MapFragment.java
+++ app/src/main/java/com/jyt/baseapp/view/fragment/MapFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
 import com.jyt.baseapp.R;
 
 /**
@@ -13,10 +15,37 @@ import com.jyt.baseapp.R;
  */
 public class MapFragment extends BaseFragment {
 
+    private MapView mMapView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        mMapView= (MapView) rootView.findViewById(R.id.mapview_map);
+        mMapView.onCreate(savedInstanceState);
+        AMap map=mMapView.getMap();
+        map.getUiSettings().setZoomControlsEnabled(false);//隐藏缩放按钮
         return rootView;
     }
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+
 }
